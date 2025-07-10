2b244f661eaabca62fe1414c628ea8e1c3569254
João Paulo Paggi <nullifidianz@gmail.com>
2025-07-10 15:51:00 -0300

    Adiciona exibição de banner na interface de linha de comando e melhora o tratamento de erros para opções inválidas.

--- src/main/java/com/githubuseractivity/cli/CommandLineInterface.java
+++ src/main/java/com/githubuseractivity/cli/CommandLineInterface.java
@@ -3,6 +3,10 @@ package com.githubuseractivity.cli;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Help.Ansi;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
 
 @Command(name = "github-user-activity", mixinStandardHelpOptions = true, version = "1.0", description = "Exibe a atividade de um usuário do GitHub.")
 public class CommandLineInterface implements Runnable {
@@ -12,12 +16,33 @@ public class CommandLineInterface implements Runnable {
 
     @Override
     public void run() {
+        exibirBanner();
         // TODO: Chamar GitHubApiClient e exibir eventos do usuário
-        System.out.println("Demo: CLI executado com sucesso para o usuário: " + username);
+        System.out
+                .println(Ansi.ON.string("@|bold,green Demo: CLI executado com sucesso para o usuário:|@ ") + username);
+    }
+
+    private void exibirBanner() {
+        try {
+            String banner = new String(Files.readAllBytes(Paths.get("src/main/resources/banner.txt")));
+            System.out.println(Ansi.ON.string("@|bold,blue " + banner + "|@"));
+        } catch (IOException e) {
+            System.out.println(Ansi.ON.string("@|red Não foi possível carregar o banner.|@"));
+        }
     }
 
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new CommandLineInterface()).execute(args);
-        System.exit(exitCode);
+        while (true) {
+            int exitCode = new CommandLine(new CommandLineInterface())
+                    .setColorScheme(CommandLine.Help.defaultColorScheme(CommandLine.Help.Ansi.ON))
+                    .execute(args);
+            if (exitCode == 0)
+                break;
+            System.out.println(Ansi.ON
+                    .string("@|bold,red Opção inválida ou argumento faltando. Tente novamente ou use --help.|@"));
+            System.out.print(Ansi.ON.string("@|yellow github-activity |> |@"));
+            args = new java.util.Scanner(System.in).nextLine().split(" ");
+
+        }
     }
 }
